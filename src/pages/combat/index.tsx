5aaafb33f8709ffd9e9b79e7683f7d73a4fa4237
Daniel Grondin <king@numsgil.co>
2022-05-16 10:15:50 -0400

    Managed to get combatant editing to work

--- src/pages/combat/index.tsx
+++ src/pages/combat/index.tsx
@@ -1,15 +1,16 @@
 import {Outlet, Route} from "@tanstack/react-location";
 import {useLiveQuery} from "dexie-react-hooks";
 import {Helmet} from "react-helmet-async";
-import {useForm} from "react-hook-form";
-import {FC, useState} from "react";
+import {set, useForm} from "react-hook-form";
+import {FaPen} from "react-icons/all";
+import {FC, useEffect, useState} from "react";
 import {
 	Button,
 	ButtonGroup,
 	Flex,
 	FormControl,
 	FormErrorMessage,
-	FormLabel,
+	FormLabel, IconButton,
 	Input,
 	Modal,
 	ModalBody,
@@ -33,30 +34,112 @@ import {DiceFormula} from "components/DiceFormula";
 import {RouteGenerics} from "../index";
 import {Encounter, ICombatant, pfdb} from "data";
 
+interface CombatantFormProps {
+	combatantId: number;
+}
+
+const CombatantFormModal: FC<CombatantFormProps & UseModalProps> = ({combatantId, ...props}) => {
+	const {register, reset, clearErrors, handleSubmit, formState: {errors, isSubmitting}} = useForm<ICombatant>();
+
+	useEffect(() => {
+		clearErrors();
+		if (combatantId === 0) {
+			reset({
+				id: undefined,
+				name: "",
+				initiative: 0,
+				maxHealth: 1,
+				type: "ally",
+			});
+		} else {
+			pfdb.combatants.get(combatantId).then(reset).catch(console.error);
+		}
+	}, [combatantId]);
+
+	async function onSubmit(value: ICombatant) {
+		if (value.id === undefined) {
+			await pfdb.combatants.add(value);
+		} else {
+			await pfdb.combatants.update(value.id, value);
+		}
+		props.onClose();
+	}
+
+	return <Modal autoFocus={false} size="md" {...props}>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Create a new Combatant</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<FormControl isInvalid={errors.name !== undefined}>
+						<FormLabel htmlFor="name">Name</FormLabel>
+						<Input
+							type="text"
+							id="name"
+							{...register("name", {
+								required: "This is required",
+							})}
+						/>
+						<FormErrorMessage>
+							{errors.name && errors.name.message}
+						</FormErrorMessage>
+					</FormControl>
+					<FormControl isInvalid={errors.initiative !== undefined}>
+						<FormLabel htmlFor="initiative">Initiative Modifier</FormLabel>
+						<NumberInput
+							min={-20}
+							max={20}
+							id="initiative"
+							{...register("initiative")}
+						>
+							<NumberInputField />
+							<NumberInputStepper>
+								<NumberIncrementStepper />
+								<NumberDecrementStepper />
+							</NumberInputStepper>
+						</NumberInput>
+					</FormControl>
+					<FormControl isInvalid={errors.maxHealth !== undefined}>
+						<FormLabel htmlFor="maxHealth">Maximum HP</FormLabel>
+						<NumberInput
+							min={1}
+							id="maxHealth"
+							{...register("maxHealth")}
+						>
+							<NumberInputField />
+							<NumberInputStepper>
+								<NumberIncrementStepper />
+								<NumberDecrementStepper />
+							</NumberInputStepper>
+						</NumberInput>
+					</FormControl>
+					<FormControl isInvalid={errors.type !== undefined}>
+						<FormLabel htmlFor="type">Maximum HP</FormLabel>
+						<Select id="type" {...register("type")}>
+							<option value="ally">Ally</option>
+							<option value="enemy">Enemy</option>
+						</Select>
+					</FormControl>
+				</ModalBody>
+				<ModalFooter>
+					<Button type="submit" colorScheme="blue" isLoading={isSubmitting}>
+						Save
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</form>
+	</Modal>;
+};
+
 interface CombatantsModalProps {
 	selectCombatant: (id: number) => void;
 }
 
 const CombatantsModal: FC<CombatantsModalProps & UseModalProps> = ({selectCombatant, ...props}) => {
-	const {register, reset, clearErrors, handleSubmit, formState: {errors, isSubmitting}} = useForm<ICombatant>();
 	const combatants = useLiveQuery(() => pfdb.combatants.toArray());
-	const {isOpen, onOpen, onClose} = useDisclosure({
-		onOpen() {
-			clearErrors();
-			reset({
-				id: undefined,
-				name: "",
-				type: "ally",
-				initiative: 0,
-				maxHealth: 1,
-			});
-		}
-	});
-
-	async function onSubmit(value: ICombatant) {
-		await pfdb.combatants.add(value);
-		onClose();
-	}
+	const [editing, setEditing] = useState<number>(0);
+	const {isOpen, onOpen, onClose} = useDisclosure();
 
 	return <>
 		<Modal size="lg" autoFocus={false} {...props}>
@@ -72,86 +155,25 @@ const CombatantsModal: FC<CombatantsModalProps & UseModalProps> = ({selectCombat
 						key={value.id}
 					>
 						<Text>{value.name} - {value.maxHealth}HP ({value.initiative > 0 ? "+" : ""}{value.initiative}) - {value.type}</Text>
-						<Button size="sm" variant="outline" onClick={() => selectCombatant(value.id ?? 0)}>
-							Add to Encounter
-						</Button>
+						<ButtonGroup isAttached size="sm" variant="outline">
+							<IconButton aria-label="Edit" icon={<FaPen />} onClick={() => { setEditing(value.id ?? 0); onOpen();}} />
+							<Button size="sm" variant="outline" onClick={() => selectCombatant(value.id ?? 0)}>
+								Add to Encounter
+							</Button>
+						</ButtonGroup>
 					</Flex>)}
 				</ModalBody>
 				<ModalFooter>
 					<Button colorScheme="blue" mr={3} onClick={props.onClose}>
 						Close
 					</Button>
-					<Button variant="ghost" onClick={onOpen}>
+					<Button variant="ghost" onClick={() => { setEditing(0); onOpen();}}>
 						Create Combatant
 					</Button>
 				</ModalFooter>
 			</ModalContent>
 		</Modal>
-		<Modal autoFocus={false} size="md" isOpen={isOpen} onClose={onClose}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Create a new Combatant</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<FormControl isInvalid={errors.name !== undefined}>
-							<FormLabel htmlFor="name">Name</FormLabel>
-							<Input
-								type="text"
-								id="name"
-								{...register("name", {
-									required: "This is required",
-								})}
-							/>
-							<FormErrorMessage>
-								{errors.name && errors.name.message}
-							</FormErrorMessage>
-						</FormControl>
-						<FormControl isInvalid={errors.initiative !== undefined}>
-							<FormLabel htmlFor="initiative">Initiative Modifier</FormLabel>
-							<NumberInput
-								min={-20}
-								max={20}
-								id="initiative"
-								{...register("initiative")}
-							>
-								<NumberInputField />
-								<NumberInputStepper>
-									<NumberIncrementStepper />
-									<NumberDecrementStepper />
-								</NumberInputStepper>
-							</NumberInput>
-						</FormControl>
-						<FormControl isInvalid={errors.maxHealth !== undefined}>
-							<FormLabel htmlFor="maxHealth">Maximum HP</FormLabel>
-							<NumberInput
-								min={1}
-								id="maxHealth"
-								{...register("maxHealth")}
-							>
-								<NumberInputField />
-								<NumberInputStepper>
-									<NumberIncrementStepper />
-									<NumberDecrementStepper />
-								</NumberInputStepper>
-							</NumberInput>
-						</FormControl>
-						<FormControl isInvalid={errors.type !== undefined}>
-							<FormLabel htmlFor="type">Maximum HP</FormLabel>
-							<Select id="type" {...register("type")}>
-								<option value="ally">Ally</option>
-								<option value="enemy">Enemy</option>
-							</Select>
-						</FormControl>
-					</ModalBody>
-					<ModalFooter>
-						<Button type="submit" colorScheme="blue" isLoading={isSubmitting}>
-							Save
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</form>
-		</Modal>
+		<CombatantFormModal combatantId={editing} isOpen={isOpen} onClose={onClose} />
 	</>;
 };
 
