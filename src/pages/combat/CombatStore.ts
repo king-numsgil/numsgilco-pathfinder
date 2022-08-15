0347f9b0d93b25dc0c9d8e641397a619c024466e
Daniel Grondin <king@numsgil.co>
2022-08-15 10:35:02 -0400

    Removed encounter from combat store (will store this info in Party)

--- src/pages/combat/CombatStore.ts
+++ src/pages/combat/CombatStore.ts
@@ -2,22 +2,13 @@ import {useCallback, useState, PropsWithChildren} from "react";
 import {state} from "@bit-about/state";
 
 interface CombatStore {
-	encounterId: number;
 	partyId: number;
-
-	loadEncounter(id: number): Promise<void>;
 
 	loadParty(id: number): Promise<void>;
 }
 
 export const [CombatStoreProvider, useCombatStore] = state<PropsWithChildren<{}>, CombatStore>((): CombatStore => {
-	const [encounterId, setEncounterId] = useState<number>(parseInt(localStorage.getItem("encounterId") ?? "0"));
 	const [partyId, setPartyId] = useState<number>(parseInt(localStorage.getItem("partyId") ?? "0"));
-
-	const loadEncounter = useCallback(async (id: number): Promise<void> => {
-		setEncounterId(id);
-		localStorage.setItem("encounterId", id.toString());
-	}, [setEncounterId]);
 
 	const loadParty = useCallback(async (id: number): Promise<void> => {
 		setPartyId(id);
@@ -25,9 +16,7 @@ export const [CombatStoreProvider, useCombatStore] = state<PropsWithChildren<{}>
 	}, [setPartyId]);
 
 	return {
-		encounterId,
 		partyId,
-		loadEncounter,
 		loadParty,
 	}
 });
